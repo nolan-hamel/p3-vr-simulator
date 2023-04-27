cb6088ce28ba61a6e5e49b832b068df63c5350a8
Asa Adomatis <asaa@clemson.edu>
2023-04-27 16:13:25 -0400

    Fixed Teleporting and Moving Around

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (agent.hasPath)
+        if (!agent.hasPath)
         {
             RandDest();
         }
